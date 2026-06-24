dfe8fa8aeba04b7afca0885e333090a13d9045b5
speedbox-frontend <vinay.kumar@speedboxapp.com>
2026-06-24 16:08:07 +0530

    feat: add next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  compress: true,
+  poweredByHeader: false,
+  generateEtags: true,
+  onDemandEntries: {
+    maxInactiveAge: 60 * 60 * 1000,
+    pagesBufferLength: 5,
+  },
+};
+
+module.exports = nextConfig;
